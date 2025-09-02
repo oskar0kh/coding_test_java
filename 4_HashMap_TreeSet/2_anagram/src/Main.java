f93fab71c2a4c07eb6063459664d44ad05360bf6
oskar0kh <oskar0kh@g.skku.edu>
2025-09-02 12:02:57 +0900

    공간 복잡도 효율 증가시킨 버전 (map 1개만 사용)

--- 4_HashMap_TreeSet/2_anagram/src/Main.java
+++ 4_HashMap_TreeSet/2_anagram/src/Main.java
@@ -3,40 +3,27 @@ import java.util.Scanner;
 
 public class Main {
     public String solution(String str1, String str2) {
-        String answer = " ";
+        String answer = "YES";
 
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
 
-        // 1. str1, str2의 알파벳 개수 세기
+        // 1. str1의 알파벳 개수 세기
         for(char x : str1.toCharArray()) {
-            map1.put(x, map1.getOrDefault(x, 0) + 1);
+            map.put(x, map.getOrDefault(x, 0) + 1);
         }
 
+        // 2. str2의 알파벳 개수 세면서, map에 똑같은 Key 있으면 Value 1 감소
         for(char x : str2.toCharArray()) {
-            map2.put(x, map2.getOrDefault(x, 0) + 1);
+            if(map.containsKey(x)) map.put(x, map.get(x) - 1);
         }
 
-        // 2. str1, str2의 key값에 따른 Value값이 똑같은지 비교
-        //    1) map1의 크기 확인
-        //    2) 먼저, map1의 Key를 map2도 가지고 있는지 확인
-        //    3) 그 다음, 'map1 Key의 Value값 == map2 Key의 Value값' 일때, count++
-        //    4) for문 종료 후, count == size 확인
-
-        int size = map1.size();
-        int count = 0;
-
-        for(char x : map1.keySet()) {
-            if(map2.containsKey(x)) { // str2에 해당 Key값이 있을때
-                if(map1.get(x) == map2.get(x)) count++;
-            }
-            else {                    // str2에 해당 Key값이 없을때
+        // 3. map의 전체 Value 값이 0이면, answer == YES
+        for(char x : map.keySet()) {
+            if(map.get(x) != 0) {
                 answer = "NO";
+                break;
             }
         }
-
-        if(count == size) answer = "YES";
-        else answer = "NO";
 
         return answer;
     }
