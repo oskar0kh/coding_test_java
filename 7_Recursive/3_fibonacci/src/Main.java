69a54b8ab92a168f52c78ff18c98bde157835175
oskar0kh <oskar0kh@g.skku.edu>
2025-10-08 12:35:08 +0900

    메모이제이션 활용

--- 7_Recursive/3_fibonacci/src/Main.java
+++ 7_Recursive/3_fibonacci/src/Main.java
@@ -1,11 +1,13 @@
 import java.io.*;
 
 public class Main {
+    static int[] fibo; // 메모이제이션용 전역 배열 생성 (모든 메소드가 이 배열 공유함)
+
     public int fibonacci(int n) {
-        if(n==1) return 1;
-        else if(n==2) return fibonacci(n-1);
+        if(fibo[n] != 0) return fibo[n]; // 기존에 저장된 결과값 있으면, 그 값 바로 반환
+        else if(n==1 || n==2) return fibo[n] = 1;
         else {
-            return fibonacci(n-2) + fibonacci(n-1);
+            return fibo[n] = fibonacci(n-2) + fibonacci(n-1); // n일때의 재귀 결과값을 배열에 저장
         }
     }
 
@@ -17,8 +19,11 @@ public class Main {
 
         Main T = new Main();
 
+        fibo = new int[n+1]; // fibonacci 메소드 실행하기 전에, fibo 전역 배열을 먼저 초기화 해줘야지 값이 여기에 저장됨
+        T.fibonacci(n);
+
         for(int i=1; i<=n; i++) {
-            bw.write(String.valueOf(T.fibonacci(i)) + " ");
+            bw.write(String.valueOf(fibo[i]) + " ");
         }
 
         bw.flush();
